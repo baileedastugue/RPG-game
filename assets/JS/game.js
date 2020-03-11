79e8496c246cb6b3d41fbd29d0c283bf6e10bfd6
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-11 15:58:11 -0500

    bootstrap works - moved position of tag in html head

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -46,14 +46,18 @@
         defeated: false
     };
 
+    // arrays to hold the dinosaurs and their corresponding images
     var characterArray = [dino1, dino2, dino3, dino4];
+    var imageHolder = ["assets/images/dino1.png", "assets/images/dino2.png", "assets/images/dino3.png", "assets/images/dino4.png"]
 
+    // booleans to change the flow of the game
     var userDecisions = true;
     var fighterChosen = false;
     var opponentChosen = false;
     var gameStarted = false;
     var gameReset = false;
 
+    // global variables to be used throughout the game
     var chosenFighter = "";
     var chosenOpponent = "";
     var fighterHP = "";
@@ -65,6 +69,7 @@
     var opponentNumber;
     var numDefeated = 0;
 
+    // references the health bars for both characters
     let opponentHealth = document.getElementById("opponent-health");
     let fighterHealth = document.getElementById("fighter-health")
     
@@ -73,6 +78,7 @@
     $("#restart").hide();
     $("#attack").hide();
 
+    // function that updates the points displayed
     function displayPoints () {
         $("#opponent-HP").html("HP: " + opponentHP);
         $("#opponent-CA").html("Counter-Attack Power: " + opponentCA);
@@ -83,21 +89,28 @@
     // function for choosing an opponent - will be called after each dinosaur is defeated
     function chooseOpponent() {
         if (!opponentChosen) {
+            // hides items that are not relevent during dino selection
             $("#dino-container").show();
             $("#attack").hide();
             $("#user-prompt").show();
             $("#user-prompt").html("Choose your next opponent:");
-             // when the user chooses their dinosaurs
+             
             for (var i = 0; i < characterArray.length; i++) {
                 if (characterArray[i].reference === userChoice && !characterArray[i].isFighter
                     && !characterArray[i].defeated) {
+                    // assigns user selection to the current opponent
                     chosenOpponent = characterArray[i];
                     chosenOpponent.isCurrentOpponent = true;
                     opponentChosen = true;
                     opponentNumber = i;
+                    // adds dinosaur image to the current opponent container
                     $("#current-opponent").prepend("<img src=" + imageHolder[i] + " width='400px'  id='opponent' class='current-dino'>");
+                    
+                    // removes opponent card from future opponent selection
                     $(chosenOpponent.cardID).removeClass("col-3");
                     $(chosenOpponent.cardID).addClass("remove-text");
+
+                    
                     opponentHealth.value = chosenOpponent.healthPoints;
                     opponentHealth.max = chosenOpponent.healthPoints;
                     $("#opponent-name").html(chosenOpponent.name);
@@ -106,13 +119,11 @@
                 }
             }        
         }
+        // hides user prompt after opponent has been selected
         if (opponentChosen) {
             $("#user-prompt").hide();
         }
     }
-    
-
-    var imageHolder = ["assets/images/dino1.png", "assets/images/dino2.png", "assets/images/dino3.png", "assets/images/dino4.png"]
 
     // function for choosing user's fighting dino - will only be called when game is reset
     function chooseFightingDino () {
@@ -135,13 +146,14 @@
         }
     }
 
-
+    // alerts user that current game is over
     function gameOver () {
         if (gameReset) {
         alert(chosenFighter.name + " met their match - click 'restart' to try again");
         }
     }
 
+    // 
     $("#restart").on("click", function () {
         window.location.reload();
     })
